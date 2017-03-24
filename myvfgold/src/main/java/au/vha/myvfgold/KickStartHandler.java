bea8929017ac7b8fe03ef2a4283be69aae7fcff5
Jp <Jp@192.168.0.2>
2017-03-25 00:13:40 +0530

    test

---
+++ myvfgold/src/main/java/au/vha/myvfgold/KickStartHandler.java
@@ -0,0 +1,14 @@
+package au.vha.myvfgold;
+
+import java.util.Calendar;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class KickStartHandler implements RequestHandler<String, String> {
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log("AWS Request ID: " + context.getAwsRequestId());
+        context.getLogger().log("Input: " + input + " at " + Calendar.getInstance().getTimeInMillis());
+        return "Cheers....We have started....Let it GO................----> ";
+    }
+}
