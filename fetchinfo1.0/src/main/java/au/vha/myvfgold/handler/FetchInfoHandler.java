69237355f7075d0db646a441006ffd4413dc77d9
jp <jp@10.144.158.142>
2017-04-04 12:29:21 +0530

    Test

--- fetchinfo1.0/src/main/java/au/vha/myvfgold/handler/FetchInfoHandler.java
+++ fetchinfo1.0/src/main/java/au/vha/myvfgold/handler/FetchInfoHandler.java
@@ -46,37 +46,5 @@ public class FetchInfoHandler implements RequestHandler<GetInfoRequest, String>
         return responseJson;
     }
 
-//    public String handleRequest(Object getInfoRequest1, Context arg1)
-//    {
-//        String responseJson = null;
-//        RequestValidator requestValidator = new RequestValidator();
-//        GetInfoRequest getInfoRequest = (GetInfoRequest) getInfoRequest1;
-//        if(requestValidator.isValidMsisdn(getInfoRequest.getMsisdn()))
-//        {
-//            GetInfoResponse getInfoResponse = new GetInfoResponse();
-//            getInfoResponse.setMsisdn(getInfoRequest.getMsisdn());
-//            getInfoResponse.setName("Lamda POST Tester");
-//            getInfoResponse.setVoiceUsage(new BigDecimal(100));
-//            getInfoResponse.setDataUsage(new BigDecimal(100.2D));
-//            ObjectMapper mapper = new ObjectMapper();
-//            try
-//            {
-//                responseJson = mapper.writeValueAsString(getInfoResponse);
-//            }
-//            catch(JsonProcessingException e)
-//            {
-//                e.printStackTrace();
-//            }
-//            catch(IOException e)
-//            {
-//                e.printStackTrace();
-//            }
-//        } else
-//        {
-//            responseJson = "{\"statusCode\":100,\"statusMessage\":\"Invalid Msisdn\"}";
-//        }
-//        return responseJson;
-//    }
-
 
 }
